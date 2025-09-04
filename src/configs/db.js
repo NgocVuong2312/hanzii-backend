dee9d1f638f503d71192af538bd32444c7f5936b
NgocVuong2312 <11236236@st.neu.edu.vn>
2025-09-04 16:47:37 +0700

    fix: update gitignore

--- src/configs/db.js
+++
@@ -1,9 +0,0 @@
-const mySql = require("mysql2/promise")
-const mySqlPool =  mySql.createPool({
-    host:"localhost",
-    user:"root",
-    password:"Vuong123!",
-    database:"Hanzii"
-})
-
-module.exports= mySqlPool
